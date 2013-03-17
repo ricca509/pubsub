406607e02d25b2fdf6d89ef3f7d817fdc5c597b0
Riccardo Coppola <coppola.riccardo@gmail.com>
2013-03-17 23:57:09 +0100

    Gruntfile cleaned

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
@@ -8,10 +7,8 @@ module.exports = function(grunt) {
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    // Default task(s).
     grunt.registerTask('default', ['jshint']);
 
 };
